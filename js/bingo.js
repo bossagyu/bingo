c899c5a0ef73ad0c3bb822fff84fd0c3c2eadc25
kouhei <feel-the-wind.sakura@hotmail.co.jp>
2015-12-14 02:10:32 +0900

    八百長機能を追加

---
+++ js/bingo.js
@@ -0,0 +1,90 @@
+$ = function(x) {
+  return document.getElementById(x);
+}
+
+// ビンゴ用数字配列
+var numList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
+var bingo_count  = 0;
+
+var isStop = true;
+
+function startBingo() {
+  // ボタンの表示切り替え
+  $("start").style.display = "none";
+  $("stop").style.display = "inline";
+  isStop = false;
+  roulette();
+}
+
+function stopBingo() {
+  // ボタンの表示切り替え
+  $("start").style.display = "inline";
+  $("stop").style.display = "none";
+  isStop = true;
+}
+
+function roulette() {
+  var id = "";
+  var rnd = Math.floor(Math.random() * numList.length);
+  // ストップボタンが押された
+  if (isStop) {
+    // 遅延呼び出しを解除
+    clearTimeout(id);
+    
+    // 特定の回数のみ、指定した数字を表示
+    // 回数に引っかかれば残りの処理をスキップする。
+    if(yaotyo()) {
+        return false;
+    }
+
+    $("view").innerHTML = numList[rnd];
+    if (!$("out").innerHTML) {
+      $("out").innerHTML = $("out").innerHTML + numList[rnd];
+    }
+    else {
+      $("out").innerHTML = $("out").innerHTML + "　" + numList[rnd];
+    }
+    
+    //決定した数字をリストから削除する
+    numList.splice(rnd, 1);
+    // リストが空になったら終了
+    if (numList.length == 0) {
+      alert("最後です。");
+      $("start").disabled = true;
+    }
+    return false;
+  }
+  
+  // 乱数を画面に表示
+  //document.write(rnd) 
+  $("view").innerHTML = numList[rnd];
+  // 100ms後に再帰的に実行するよう登録する
+  id = setTimeout("roulette()", 100);
+
+}
+
+// 特定の回数のときだけ八百長します。
+// 数値のリストから八百長の数字については抜いておいてください
+function yaotyo() {
+    var num = "";
+
+    switch (bingo_count) {
+        case 1:
+            num = 12;
+            $("view").innerHTML = num;
+            $("out").innerHTML = $("out").innerHTML + "　" + num;
+            break;
+        case 4:
+            num = 15;
+            $("view").innerHTML = num;
+            $("out").innerHTML = $("out").innerHTML + "　" + num;
+            break;
+        default: 
+            bingo_count++;
+            return (false);
+    }
+    bingo_count++;
+    return(true);
+
+}
+
